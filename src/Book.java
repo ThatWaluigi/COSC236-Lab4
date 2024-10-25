0741f27877d30704407e36fca7a46b4cd7b1e780
ThatWaluigi <ThatWaluigi@gmail.com>
2024-10-25 15:20:00 -0700

    INIT

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+
+	// GRASP Principle: Information Expert
+	// Manages its own state (available or borrowed).
+	private String title;
+	private boolean isAvailable; 
+	
+	// TODO: implement Book functionality 
+	
+}
