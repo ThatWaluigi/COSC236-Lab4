929140b49f67fb429daab291f73f94ac15a480ac
aymanBH04 <aay4584@gmail.com>
2024-10-25 16:03:08 -0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -6,8 +6,33 @@ public class Book {
 	private String author;
 	private boolean isAvailable; 
 	
-	// TODO: implement Book functionality 
+	//We declare the title, author and if It's available
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+		this.isAvailable = true;
+	}
 	
+	
+		//method so we get the title
+		public String getTitle() {
+			return title;
+		}
+		//method to get the author
+		public String getAuthor() {
+			return author;
+		}
+		//method to see if It's available
+		public boolean isAvailable() {
+			return true;
+		}
+		
+		public void borrow() {
+			if(isAvailable) {
+				isAvailable = false;
+				
+			}
+		}
 	
 	
 }
