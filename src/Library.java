3cf83f42d8e51e6b363dfde42e78def5b6bf8b76
ThatWaluigi <ThatWaluigi@gmail.com>
2024-10-25 16:15:12 -0700

    Library Function Name Refactor
    Refactored book.getName -> book.getTitle

--- src/Library.java
+++ src/Library.java
@@ -37,27 +37,27 @@ public class Library {
 	public void addBook(Book book){
 		if (!catalog.contains(book)){
 			catalog.add(book);
-			System.out.println(book.getName() + " has been added to the catalog.");
+			System.out.println(book.getTitle() + " has been added to the catalog.");
 		}
 	}
 
 	public void removeBook(Book book){
 		catalog.remove(book);
-		System.out.println(book.getName() + " has been removed from the catalog.");
+		System.out.println(book.getTitle() + " has been removed from the catalog.");
 	}
 
 	public void borrowBook(Member member, Book book){
 		if (!borrowedBooks.contains(book)){
 			member.borrowBook(book);
 			borrowedBooks.add(book);
-			System.out.println(member.getName() + " has borrowed the book '" + book.getName() + "'");
+			System.out.println(member.getName() + " has borrowed the book '" + book.getTitle() + "'");
 		}
 	}
 
 	public void returnBook(Member member, Book book){
 		if (borrowedBooks.contains(book)){
 			member.returnBook(book);
-			System.out.println(member.getName() + " has returned the book '" + book.getName() + "'");
+			System.out.println(member.getName() + " has returned the book '" + book.getTitle() + "'");
 		}
 	}
 
@@ -65,7 +65,7 @@ public class Library {
 		System.out.println("Available Books: ");
 		for (Book book : catalog){
 			if (!borrowedBooks.contains(book)){
-				System.out.println("	" + book.getName());
+				System.out.println("	" + book.getTitle());
 			}
 		}
 	}
